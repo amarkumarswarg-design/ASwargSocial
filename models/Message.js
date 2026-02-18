470d1d01a2170de968b090660223166a3840f6c8
amarkumarswarg-design <amarkumarswarg@gmail.com>
2026-02-18 20:48:12 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
+
 const messageSchema = new mongoose.Schema({
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  content: { type: String, required: true },
+  content: { type: String, trim: true },
+  media: [{
+    url: String,
+    publicId: String,
+    type: String
+  }],
   readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Message', messageSchema);
