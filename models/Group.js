ad7ffbdfaa464322bc9481b702c45f5b93786c46
amarkumarswarg-design <amarkumarswarg@gmail.com>
2026-02-18 22:28:32 +0530

    Create Group.js

---
+++ models/Group.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dp: { type: String, default: '' },
+  dpPublicId: { type: String, default: '' },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Group', groupSchema);
